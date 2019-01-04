6beccfff207638bcc0f6af0579c55bbc42ad2d9b
Andrew Byrd <byrd286@gmail.com>
2019-01-04 14:57:56 -0500

    customized for me

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-It's me, Tierney!
+npx business card, thx @bitandbang
 
 # Usage
-Via npx:
-```
-npx bitandbang
-```
 
-Not including how to use it globally because I'm not sure why you'd want this as a global command. That'd be creepy.
+Via npx:
+
+```bash
+npx wordythebyrd
+```
--- build.js
+++ build.js
@@ -8,23 +8,23 @@ const path = require('path')
 
 // Define options for Boxen
 const options = {
-  padding: 1,
+  padding: 2,
   margin: 1,
-  borderStyle: 'round'
+  borderStyle: 'double'
 }
 
 // Text + chalk definitions
 const data = {
-  name: chalk.white('               Tierney Cyren'),
-  handle: chalk.white('bitandbang'),
-  work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
-  opensource: chalk.white('Node.js Community Committee ') + chalk.green('⬢'),
-  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('bitandbang'),
-  npm: chalk.gray('https://npmjs.com/') + chalk.red('~bnb'),
-  github: chalk.gray('https://github.com/') + chalk.green('bnb'),
-  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('bitandbang'),
-  web: chalk.cyan('https://bnb.im'),
-  npx: chalk.red('npx') + ' ' + chalk.white('bitandbang'),
+  name: chalk.white('               Andrew Byrd'),
+  handle: chalk.white('wordythebyrd'),
+  work: chalk.white('Senior Software Engineer at PayScale'),
+  opensource: chalk.white('Massive Nerds - RetroMatic'),
+  twitter: chalk.gray('https://twitter.com/') + chalk.cyan('wordythebyrd'),
+  npm: chalk.gray('https://npmjs.com/') + chalk.cyan('~wordythebyrd'),
+  github: chalk.gray('https://github.com/') + chalk.cyan('wordythebyrd'),
+  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.cyan('wordythebyrd'),
+  web: chalk.cyan('https://wordythebyrd.github.io/'),
+  npx: chalk.yellow('npx wordythebyrd'),
   labelWork: chalk.white.bold('       Work:'),
   labelOpenSource: chalk.white.bold('Open Source:'),
   labelTwitter: chalk.white.bold('    Twitter:'),
@@ -59,4 +59,4 @@ const output = heading + // data.name + data.handle
                webing + newline + newline + // data.labelWeb + data.web
                carding // data.labelCard + data.npx
 
-fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)))
+fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.magenta(boxen(output, options)))
